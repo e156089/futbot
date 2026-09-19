14b7b314f693044ec23b0d86d3ddb2ff94771e30
e156089 <e156089@gmail.com>
2026-09-18 23:45:47 -0300

    Ahora el reporte de partidos no se rompe al haber partidos en vivo, entretiempo o con jornadas por la mitad

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord, os, datetime
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
+from reporte import obtener_partidos, obtener_tabla
 
 load_dotenv()
 TOKEN_BOT = os.environ.get("TOKEN_BOT")
@@ -15,39 +16,62 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     print(f'Conectado')
+    # buscar_info()
+    # a = obtener_reporte()
+    # if not reporte.is_running(): # Empezar loop
+    #     reporte.start()
 
-    if not reporte.is_running(): # Empezar loop
-        reporte.start()
 
-
-hora = datetime.time(hour=0, minute=0)
-@tasks.loop(time=hora)
-async def reporte():
-    hoy = datetime.datetime.now()
-    # 0 = Lunes | 1 = Martes | 2 = Miércoles | 3 = Jueves 
-    # 4 = Viernes | 5 = Sábado | 6 = Domingo
-    if hoy.weekday() == 1:
-        ID_DEL_CANAL = 1398376928716652697
-        canal = bot.get_channel(ID_DEL_CANAL)
-        if canal:
-            try:
-                from reporte import obtener_reporte
-                contenido = obtener_reporte()
-                await canal.send(embed=contenido) # type: ignore
-            except Exception as e:
-                await canal.send(embed=alerta("reporte", e)) # type: ignore
+# hora = datetime.time(hour=0, minute=0)
+# @tasks.loop(time=hora)
+# async def reporte():
+#     hoy = datetime.datetime.now()
+#     # 0 = Lunes | 1 = Martes | 2 = Miércoles | 3 = Jueves 
+#     # 4 = Viernes | 5 = Sábado | 6 = Domingo
+#     if hoy.weekday() == 1:
+#         ID_DEL_CANAL = 1398376928716652697
+#         canal = bot.get_channel(ID_DEL_CANAL)
+#         if canal:
+#             try:
+#                 contenido = obtener_reporte()
+#                 await canal.send(embed=contenido) # type: ignore
+#             except Exception as e:
+#                 await canal.send(embed=alerta("reporte", e)) # type: ignore
 
 
 @bot.command()
 async def menu(ctx):
-    await ctx.send('Aca va a ir el futuro menu.\n**Madison**')
+    embed = discord.Embed(
+        title="**Menú:**",
+        color=discord.Color.gold()
+    )
+    embed.description = f"- **!partidos:** Obtener partidos de la fecha actual y anterior.\n"
+    embed.description+= f"- **!tabla:** Obtener la tabla de posiciones actual.\n"
+    await ctx.send(embed=embed)
 
+
+@bot.command()
+async def partidos(ctx):
+    try:
+        embed = obtener_partidos()
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.send(embed=alerta("partidos", e))
+
+
+@bot.command()
+async def tabla(ctx):
+    try:
+        embed = obtener_tabla()
+        await ctx.send(embed=embed)
+    except Exception as e:
+        await ctx.send(embed=alerta("tabla", e))
 
 
 @bot.command()
 async def test(ctx):
     try:
-        ctx.send("No hay nada que testear.")
+        await ctx.send("No hay nada que testear.")
     except Exception as e:
         await ctx.send(embed=alerta("test", e))
 
@@ -56,7 +80,7 @@ async def test(ctx):
 def alerta(nombre_funcion,e):
     embed = discord.Embed(
         title="⚠️ **ERROR**",
-        description=f"**Se llevaron preso al Chiqui Tapia.**\nFuncion -> {nombre_funcion}",
+        description=f"**Se llevaron preso al Chiqui Tapia.**",
         color=discord.Color.orange()
     )
     print(nombre_funcion, e)
--- reporte.py
+++ reporte.py
@@ -1,79 +1,123 @@
-import discord, time, undetected_chromedriver as uc
+import discord, time, json, os, undetected_chromedriver as uc
+from datetime import datetime
 from bs4 import BeautifulSoup # beautifulsoup4
 
-# # Hacer clic: elemento.click() (Es como tocar la pantalla con el dedito).
-# # Escribir: elemento.send_keys("hola") (Como tipear en el teclado).
-# # Borrar texto: elemento.clear() (Pasa una goma de borrar invisible si la caja ya tenía texto escrito).
-# robot = uc.Chrome()
+# sudo apt-get update
+# sudo apt-get install xvfb
 
-# # NAVEGAR
-# robot.get("https://www.google.com/search?q=liga+argentina+partidos&oq=liga+&gs_lcrp=EgZjaHJvbWUqCAgAEEUYJxg7MggIABBFGCcYOzIGCAEQRRg5MgoIAhAuGLEDGIAEMg0IAxAuGIMBGLEDGIAEMhAIBBAuGIMBGLEDGIAEGLQHMgoIBRAuGLEDGIAEMgcIBhAAGIAEMgcIBxAAGIAEMgoICBAuGLEDGIAEMhAICRAuGIMBGLEDGIAEGIoF0gEIMzM1OGowajSoAgCwAgA&sourceid=chrome&source=chrome.ob&ie=UTF-8#sie=lg;/g/11msmq3ggx;2;/m/04hpk1;mt;fp;1;;;;-1")
+def buscar_info(URL):
+    # Hacer clic: elemento.click() (Es como tocar la pantalla con el dedito).
+    # Escribir: elemento.send_keys("hola") (Como tipear en el teclado).
+    # Borrar texto: elemento.clear() (Pasa una goma de borrar invisible si la caja ya tenía texto escrito).
+    robot = uc.Chrome()
+    # NAVEGAR
+    robot.get(URL)
+    time.sleep(10) # Esperar q cargue la página
+    sopa = BeautifulSoup(robot.page_source, "html.parser") # Obtener html
+    robot.quit()
+    textos_html = list(sopa.stripped_strings)
+    
+    return textos_html # Lista de textos html
+    
 
-# # Esperamos hasta q vea la página
-# time.sleep(15)
+def crear_diccionario(textos_html):
+    for i in range(len(textos_html) - 1, -1, -1):
+        if "►" in textos_html[i] or textos_html[i] == textos_html[i-1]:
+            del textos_html[i]
 
-# with open("liga.html", "w", encoding="utf-8") as archivo:
-#     archivo.write(robot.page_source)
-
-# sopa = BeautifulSoup(robot.page_source, "html.parser")
-
-# with open("liga_sopa.html", "w", encoding="utf-8") as archivo:
-#     archivo.write(sopa.prettify()) # type: ignore
-
-
-def obtener_reporte():
-    with open("liga_sopa.html", "r", encoding="utf-8") as archivo:
-        sopa_html = archivo.read()
-
-    sopa = BeautifulSoup(sopa_html, "html.parser")
-
-    textos = list(sopa.stripped_strings)
-
-    for i in range(len(textos) - 1, -1, -1):
-        if "►" in textos[i]:
-            del textos[i]
-            
-    #imagenes = [imagen.get('src') for imagen in sopa.find_all('img') if imagen.get('src')]
+    cont = 0
+    for i in range(len(textos_html)):
+        if "jornada" in textos_html[i].lower():
+            cont += 1
+            if cont == 2:
+                textos_html = textos_html[i:]
+                break
 
     finalizados = {}
     proximos = {}
-    for i in range(len(textos)):
-        if "jornada" in textos[i].lower() and "fin" in textos[i+1].lower():
-            finalizados["jornada"] = textos[i]
+    ind_f = 6
+    ind_p = 4
+    # print(textos_html)
+    for i in range(len(textos_html)):
+        if "jornada" in textos_html[i].lower() and "fin" in textos_html[i+1].lower() and "fin" in textos_html[i+1 + ind_f*14].lower():
+            finalizados["jornada"] = textos_html[i]
             finalizados["partidos"] = []
             for j in range(15):
+                ind = ind_f*j
                 finalizados["partidos"].append(
                     {
-                        "estado": textos[i+1 + 8*j],
-                        "fecha": textos[i+2 + 8*j], 
+                        "estado": textos_html[i+1 + ind],
+                        "fecha": textos_html[i+2 + ind].replace("/", "-"), 
                         "equipos": [
-                            {"nombre": textos[i+3 + 8*j], "resultado":textos[i+5 + 8*j]},
-                            {"nombre": textos[i+6 + 8*j], "resultado":textos[i+8 + 8*j]}
+                            {"nombre": textos_html[i+3 + ind], "resultado":textos_html[i+4 + ind]},
+                            {"nombre": textos_html[i+5 + ind], "resultado":textos_html[i+6 + ind]}
                         ]
                     }
                 )
 
-        elif "jornada" in textos[i].lower() and "fin" not in textos[i+1].lower():
-            proximos["jornada"] = textos[i]
+        elif "jornada" in textos_html[i].lower():
+            proximos["jornada"] = textos_html[i]
             proximos["partidos"] = []
+            cal = 0 # calibracion de indice
             for j in range(15):
-                fecha = textos[i+2 + 6*j].split()
-                proximos["partidos"].append(
-                    {   
-                        "estado": textos[i+1 + 6*j],
-                        "fecha": f"{fecha[0]} {fecha[1][0]}{fecha[2][0]}",
-                        "equipos": [
-                            {"nombre": textos[i+3 + 6*j]},
-                            {"nombre": textos[i+5 + 6*j]}
-                        ]
-                    }
-                )
+                ind_add = ind_p*j+cal
+                if "fin" in textos_html[i+1 + ind_add].lower():
+                    proximos["partidos"].append(
+                        {
+                            "estado": textos_html[i+1 + ind_add],
+                            "fecha": textos_html[i+2 + ind_add].replace("/", "-"), 
+                            "equipos": [
+                                {"nombre": textos_html[i+3 + ind_add], "resultado":textos_html[i+4 + ind_add]},
+                                {"nombre": textos_html[i+5 + ind_add], "resultado":textos_html[i+6 + ind_add]}
+                            ]
+                        }
+                    )
+                    cal += 2
+                elif "vivo" in textos_html[i+1 + ind_add].lower():
+                    proximos["partidos"].append(
+                        {
+                            "estado": f"{textos_html[i+1 + ind_add]} {textos_html[i+2 + ind_add]}{textos_html[i+3 + ind_add]}",
+                            "fecha": None, 
+                            "equipos": [
+                                {"nombre": textos_html[i+4 + ind_add], "resultado":textos_html[i+5 + ind_add]},
+                                {"nombre": textos_html[i+6 + ind_add], "resultado":textos_html[i+7 + ind_add]}
+                            ]
+                        }
+                    )
+                    cal += 3
+                elif "entretiempo" in textos_html[i+1 + ind_add].lower():
+                    proximos["partidos"].append(
+                        {
+                            "estado": textos_html[i+1 + ind_add],
+                            "fecha": None, 
+                            "equipos": [
+                                {"nombre": textos_html[i+2 + ind_add], "resultado":textos_html[i+3 + ind_add]},
+                                {"nombre": textos_html[i+4 + ind_add], "resultado":textos_html[i+5 + ind_add]}
+                            ]
+                        }
+                    )
+                    cal += 1
+                else:
+                    fecha = textos_html[i+2 + ind_add].split()
+                    ultima_fecha = textos_html[i+1 + ind_add].split()
+                    proximos["partidos"].append(
+                        {   
+                            "estado": "Proximamente",
+                            "fecha": f"{textos_html[i+1 + ind_add]} {fecha[0]} {fecha[1][0]}{fecha[2][0]}",
+                            "equipos": [
+                                {"nombre": textos_html[i+3 + ind_add]},
+                                {"nombre": textos_html[i+4 + ind_add]}
+                            ]
+                        }
+                    )
             break
-        
     # print("\n")
-    # print(datos_finalizados, "\n\n")
-    # print(datos_no_iniciados)
-    
+    # print(finalizados, "\n\n")
+    # print(proximos)
+    return {"finalizados": finalizados, "proximos": proximos}
+
+
+def crear_embed(contenido):
     embed = discord.Embed(
         title="⚽ El Chiqui Tapia trajo el resumen:", 
         color=discord.Color.blue()
@@ -86,21 +130,58 @@ def obtener_reporte():
             eq1 = p['equipos'][0]['nombre']
             eq2 = p['equipos'][1]['nombre']
             
-            if p.get('estado') == 'Fin':
+            if "fin" in p.get('estado').lower():
+                info = f"` {p['equipos'][0]['resultado']} - {p['equipos'][1]['resultado']} `"
+            elif "vivo" in p.get('estado').lower():
+                eq1 = f"{p.get("estado")}:  {eq1}"
+                info = f"` {p['equipos'][0]['resultado']} - {p['equipos'][1]['resultado']} `"
+            elif "entretiempo" in p.get('estado').lower():
+                eq1 = f"{p.get("estado")}:  {eq1}"
                 info = f"` {p['equipos'][0]['resultado']} - {p['equipos'][1]['resultado']} `"
             else:
-                info = f"` {p['estado']} {p['fecha']} `"
+                info = f"` {p['fecha']} `"
             
             lista_partidos.append(f"• **{eq1}** {info} **{eq2}**")
         
         # Agregamos toda la jornada en un único bloque de arriba a abajo (inline=False)
         embed.add_field(
-            name=f"🏆 {datos['jornada']}", 
+            name=f"\n🏆 {datos['jornada']}", 
             value="\n".join(lista_partidos), 
             inline=False
         )
         
-    agregar_jornada(finalizados)
-    agregar_jornada(proximos)
+    agregar_jornada(contenido["finalizados"])
+    agregar_jornada(contenido["proximos"])
     
     return embed
+
+
+def obtener_partidos():
+
+    # info_actualizada = False
+    # path = "liga_partidos.json"
+    # if os.path.isfile(path):
+    #     with open(path, "r", encoding="utf-8") as archivo:
+    #         contenido = json.load(archivo)
+
+    #     fecha = contenido["fecha"].strip()
+    #     fecha_guardada = datetime.strptime(fecha, "%Y-%m-%d").date()
+    #     hoy = datetime.now().date()
+
+    #     if fecha_guardada > hoy:
+    #         info_actualizada = True
+
+    # if not info_actualizada:
+    URL = "https://www.google.com/search?q=liga+argentina+partidos&oq=liga+&gs_lcrp=EgZjaHJvbWUqCAgAEEUYJxg7MggIABBFGCcYOzIGCAEQRRg5MgoIAhAuGLEDGIAEMg0IAxAuGIMBGLEDGIAEMhAIBBAuGIMBGLEDGIAEGLQHMgoIBRAuGLEDGIAEMgcIBhAAGIAEMgcIBxAAGIAEMgoICBAuGLEDGIAEMhAICRAuGIMBGLEDGIAEGIoF0gEIMzM1OGowajSoAgCwAgA&sourceid=chrome&source=chrome.ob&ie=UTF-8#sie=lg;/g/11msmq3ggx;2;/m/04hpk1;mt;fp;1;;;;-1"
+    textos_html = buscar_info(URL)
+    contenido = crear_diccionario(textos_html)
+
+        # with open(path, "w", encoding="utf-8") as archivo:
+        #     json.dump(contenido, archivo)
+            
+    embed = crear_embed(contenido)
+    return embed
+
+
+def obtener_tabla():
+    URL = "https://www.google.com/search?q=partidos+liga+argentina&oq=part&gs_lcrp=EgZjaHJvbWUqBggBECMYJzIGCAAQRRg5MgYIARAjGCcyBggCECMYJzIHCAMQABiABDIHCAQQABiABDIGCAUQRRg9MgYIBhBFGD0yBggHEEUYPNIBCDM4MjhqMGo3qAIIsAIB8QUH3g9qWDYsI_EFB94Palg2LCM&sourceid=chrome&source=chrome.ob&ie=UTF-8#sie=lg;/g/11msmq3ggx;2;/m/04hpk1;st;fp;1;;;;-1"
